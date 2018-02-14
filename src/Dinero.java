2db13ad6913152353e58b2a9bfa422d48863e0e3
Juan <juan.notario20@gmail.com>
2018-02-14 17:33:33 +0100

    Aclaración sobre el uso de un if

--- src/Dinero.java
+++ src/Dinero.java
@@ -11,31 +11,32 @@ public class Dinero {
 		int cantidad = Integer.parseInt(sc.nextLine());
 		
 		while (cantidad>0) {
+			//Si la cantidad es mayor que 100, calcula en nÃºmero de billetes de 100 necesarios.
 			if (cantidad > 100) {
 				billetes = cantidad/100;
 				resto = cantidad%100;
-				System.out.println("Son " + billetes + " de 100 €");
+				System.out.println("Son " + billetes + " de 100 ï¿½");
 				cantidad = resto;
 			
 			} else if (cantidad >= 50) {
 				billetes = resto/50;
 				resto = cantidad%50;
-				System.out.println("Son " + billetes + " de 50 €");
+				System.out.println("Son " + billetes + " de 50 ï¿½");
 				cantidad = resto;
 				
 			} else if (cantidad >= 20) {
 				billetes = resto/20;
 				resto = resto%20;
-				System.out.println("Son " + billetes + " de 20 €");
+				System.out.println("Son " + billetes + " de 20 ï¿½");
 				cantidad = resto;
 			
 			} else if (resto >= 10) {
 				billetes = resto/10;
 				resto = resto%10;
-				System.out.println("Son " + billetes + " de 10 €");
+				System.out.println("Son " + billetes + " de 10 ï¿½");
 				cantidad = resto;
 			} else {
-				System.out.println("y " + cantidad + " € sueltos en monedas");
+				System.out.println("y " + cantidad + " ï¿½ sueltos en monedas");
 				cantidad = 0;
 		sc.close();
 			}
